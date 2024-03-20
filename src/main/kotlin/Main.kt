360d42bfd5e48ac1eb106e480b781553e77de1fd
herbertdesousa <sousaherbert138@gmail.com>
2024-03-20 17:56:14 -0300

    FEAT: open window

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,35 @@
 package org.example
 
+import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.glfw.GLFWErrorCallback
+import org.lwjgl.opengl.GL
+import org.lwjgl.opengl.GL13.*
+import org.lwjgl.system.MemoryUtil.NULL
+
 fun main() {
-    println("Hello World!")
+    GLFWErrorCallback.createPrint(System.err).set()
+
+    glfwInit()
+
+    glfwDefaultWindowHints()
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3)
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
+
+    val window = glfwCreateWindow(800, 800, "Rodante", NULL, NULL)
+
+    glfwMakeContextCurrent(window)
+
+    GL.createCapabilities()
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents()
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f)
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        glfwSwapBuffers(window)
+    }
+
+    glfwTerminate()
 }
